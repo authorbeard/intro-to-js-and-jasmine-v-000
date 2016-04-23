4e5449da66948ea255aefbbb226cdf86a5d0cebe
authorbeard <msnolan@gmail.com>
2016-04-23 10:20:58 -0400

    all tests passing

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function favIceCream(fav){
+  return "I love " + fav
+}
+
+function shouting(fav){
+  var caps=fav.toUpperCase();
+  return caps;
+}
+
+function roundDown(num){
+  var floor = Math.floor(num);
+  return floor;
+}
+
+function  theTruth(deez){
+  return true; 
+
+}
